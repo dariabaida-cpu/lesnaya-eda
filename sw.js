49c2d251e060a854078ffa5d572001d744ba7bc8
dariabaida-cpu <daria.baida@gmail.com>
2026-06-12 01:25:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,23 @@
-const CACHE = 'lesnaya-eda-v1';
+const CACHE = 'lesnaya-eda-v2';
 const FILES = [
   './index.html',
+  './menu.html',
   './manifest.json',
-  './icon.png'
+  './manifest_menu.json',
+  './icon_recipe.png',
+  './icon_menu.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
